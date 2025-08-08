2b2c30191df759be3672669b2c2d231615ac75ce
Zhehao_Shen <1017495797@qq.com>
2025-08-08 23:56:39 +0800

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,8 @@ import os
 
 def get_args_parser():
     parser = argparse.ArgumentParser('Sup_model_path', add_help=False)
-    parser.add_argument('--model_dir', default=r'/supcon1/public_model/llm')
+    parser.add_argument('--model_dir', default=r'/model_path')
     parser.add_argument('--project_path', default=os.path.dirname(__file__))
     return parser
+
 
